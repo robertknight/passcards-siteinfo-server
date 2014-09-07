8d0ac526699defaa1f23ff65ce4b582b5060a469
Robert Knight <robertknight@gmail.com>
2014-09-07 12:14:14 +0100

    Use SSL certificates from /etc/ssl/certs instead of those bundled with Node
    OpenShift is currently running Node.js v0.10.25 which uses an old
    CA cert bundle by default, last updated in 2011. This resulted in secure
    connections to certain domains (eg. www.writelatex.com) failing.
    
    Use the SSL certs from /etc/ssl/certs instead.

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
+// 7/9/14 - OpenShift is currently using Node.js v0.10.25
+// which ships with an old CA cert bundle.
+//
+// Use the certificates from /etc/ssl/certs instead
+//
+var crypto_cacerts = require('crypto-cacerts');
+crypto_cacerts.cryptoPatch('/etc/ssl/certs');
+
 var server = require('./build/src/server');
 server.main();
